0f3638c071ee41ef46a3226f0512478e7061fdbf
g-priime <57049481+g-priime@users.noreply.github.com>
2020-12-31 10:11:05 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,20 @@
-import * as firebase from 'firebase/app'
-import 'firebase/storage'
-import 'firebase/firestore'
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBvnhdEiJ8sGmmeoizoGNOwtTTdW5HgJ3g",
-    authDomain: "base-firegram.firebaseapp.com",
-    projectId: "base-firegram",
-    storageBucket: "base-firegram.appspot.com",
-    messagingSenderId: "992434235186",
-    appId: "1:992434235186:web:8985ec7a438e9f0ece992b"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBvnhdEiJ8sGmmeoizoGNOwtTTdW5HgJ3g",
+  authDomain: "base-firegram.firebaseapp.com",
+  projectId: "base-firegram",
+  storageBucket: "base-firegram.appspot.com",
+  messagingSenderId: "992434235186",
+  appId: "1:992434235186:web:8985ec7a438e9f0ece992b",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const projectStorage = firebase.storage();
-  const projectFirestore = firebase.firestore();
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
 
-  export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore };
