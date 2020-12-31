64fa036b7bc266e9994f0cf971245a8ae9959f60
g-priime <57049481+g-priime@users.noreply.github.com>
2020-12-31 14:11:17 -0700

    Update ProgressBar.js

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -5,7 +5,7 @@ const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
   console.log(progress, url);
 
-  return <div className="progress-bar">progress</div>;
+  return <div className="progress-bar" style={{ width: progress + '%' }}>progress</div>;
 };
 
 export default ProgressBar;
