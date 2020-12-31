de9f24bc577b877f27f27ae1b119c51cc89f04a3
g-priime <57049481+g-priime@users.noreply.github.com>
2020-12-31 08:52:29 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,7 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBvnhdEiJ8sGmmeoizoGNOwtTTdW5HgJ3g",
@@ -9,3 +13,8 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export { projectStorage, projectFirestore };
