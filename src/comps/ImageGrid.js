19905d9c81f69739a69ddd635e989bae156a1390
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-01 01:27:26 -0700

    Update ImageGrid.js

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -5,7 +5,16 @@ const ImageGrid = () => {
   const { docs } = useFirestore("images");
   console.log(docs);
 
-  return <div className="img-grid">images</div>;
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div className="img-wrap" key={doc.id}>
+            <img src={doc.url} alt="uploaded pic" />
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ImageGrid;
