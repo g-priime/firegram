1e8c43f5e37712b0fde3b424ac76cbd1ce4e410d
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-01 01:50:08 -0700

    Update Modal.js

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Modal = () => {
+const Modal = ({ selectedImg }) => {
   return (
     <div className="backdrop">
-      <img src="" alt="enlarged pic" />
+      <img src={selectedImg} alt="enlarged pic" />
     </div>
   );
 };
