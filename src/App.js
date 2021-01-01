04e2239a0e5fe5a8960bbbdd57238ed5aea78a6c
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-01 00:55:42 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Title from './comps/Title';
-import UploadForm from './comps/UploadForm';
+import React from "react";
+import ImageGrid from "./comps/ImageGrid";
+import Title from "./comps/Title";
+import UploadForm from "./comps/UploadForm";
 
 function App() {
   return (
     <div className="App">
-      <Title/>
+      <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
