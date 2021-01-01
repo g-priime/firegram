f64992e78d872a3d8296958aebcf4fa2a6317359
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-01 02:29:39 -0700

    Update ImageGrid.js

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -17,7 +17,13 @@ const ImageGrid = ({ setSelectedImg }) => {
             whileHover={{ opacity: 1 }}
             onClick={() => setSelectedImg(doc.url)}
           >
-            <img src={doc.url} alt="uploaded pic" />
+            <motion.img
+              src={doc.url}
+              alt="uploaded pic"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
           </motion.div>
         ))}
     </div>
