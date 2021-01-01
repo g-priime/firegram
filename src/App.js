e14a9ba0a610c6e11f05eebc1c585fa278d966a3
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-01 07:26:26 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ImageGrid from "./comps/ImageGrid";
 import Modal from "./comps/Modal";
 import Title from "./comps/Title";
-import UploadForm from "./comps/UploadForm";
+/* import UploadForm from "./comps/UploadForm"; */
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
