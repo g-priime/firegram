f7bfe33fcab95dc1e91bdef70984b6916a715c03
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-06 08:25:23 -0700

    Update UploadForm.js

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -4,26 +4,42 @@ import ProgressBar from "./ProgressBar";
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
+  const [location, setLocation] = useState(null);
 
   const types = ["image/png", "image/jpeg"];
+
+  let selectedLocation = "";
+
+  const handleChange = (event) => {
+    selectedLocation = event.target.value;
+
+    //console.log(selectedLocation);
+  };
 
   const changeHandler = (e) => {
     let selected = e.target.files[0];
 
     if (selected && types.includes(selected.type)) {
       setFile(selected);
+      setLocation(selectedLocation);
       setError("");
     } else {
       setFile(null);
       setError("Please select an image file (png or jpeg)");
     }
+
+    console.log(file);
+    console.log(location);
   };
 
   return (
     <form>
       <label>
-        <input type="file" onChange={changeHandler} />
-        <span></span>
+        <input type="text" onChange={handleChange} />
+      </label>
+      <label className="file-label">
+        <input className="file-input" type="file" onChange={changeHandler} />
+        <span>+</span>
       </label>
       <div className="output">
         {error && <div className="error">{error}</div>}
