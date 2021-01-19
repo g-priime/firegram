fff3ccfbe8f345861170dad9a5f6716c23df8739
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-19 12:43:37 -0700

    Update deletePic.js

--- src/hooks/deletePic.js
+++ src/hooks/deletePic.js
@@ -1,14 +1,17 @@
-import { useState, useEffect } from "react";
-import { projectStorage, projectFirestore } from "../firebase/config";
+import { projectFirestore } from "../firebase/config";
 
 const deletePic = (fileName) => {
   const collectionRef = projectFirestore.collection("images");
 
-  collectionRef.doc(fileName).delete().then(function() {
-    console.log("Document successfully deleted!");
-}).catch(function(error) {
-    console.error("Error removing document: ", error);
-});
+  collectionRef
+    .doc(fileName)
+    .delete()
+    .then(function () {
+      console.log("Document successfully deleted!");
+    })
+    .catch(function (error) {
+      console.error("Error removing document: ", error);
+    });
 
   return { fileName };
 };
