920080fba4f8efac7dd4bdc6ba05f81cd1e4655f
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-22 07:07:54 -0700

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
       <h1>SMBD</h1>
       <h2>So Much Beauty in Dirt</h2>
+      {/*
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      */}
     </div>
-  )
-}
+  );
+};
 
 export default Title;
