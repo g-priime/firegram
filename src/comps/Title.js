e296b6b635bd41c86cf580954a4063ff0eaea806
g-priime <57049481+g-priime@users.noreply.github.com>
2021-01-24 12:50:59 -0700

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Title = () => {
+const Title = ({ setShowForm }) => {
+  const handleClick = () => {
+    setShowForm(true);
+  };
+
   return (
     <div className="title">
-      <h1>SMBD</h1>
+      <header>
+        <h1>SMBD</h1>
+        <button onClick={handleClick}>Add Photo</button>
+      </header>
       <h2>So Much Beauty in Dirt</h2>
       {/*
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
